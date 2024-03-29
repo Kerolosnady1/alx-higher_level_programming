c1626d50b25fef798fd12684554458105dd65ce7
Kerolosnady1 <kerolosnady444@gmail.com>
2024-03-29 10:25:13 -0700

    testenv

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the provided URL and displays the body of the response for a 200 status code
-curl -s -o /dev/null -w "%{http_code}" {URL} | grep 200 >/dev/null && curl -s {URL}
-
+# This script sends a GET request to a URL and displays the body of the response for a 200 status code
+curl -s $1
