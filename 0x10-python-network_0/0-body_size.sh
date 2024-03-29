6db19e30ef50911115bfd4ed64e6dd9e7a00e126
Kerolosnady1 <kerolosnady444@gmail.com>
2024-03-29 10:06:32 -0700

    testsh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+# This script sends a request to a URL and displays the size of the response body in bytes
 url=$1
-response=$(curl -sI $url | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
+response=$(curl -sI $url | grep -i Content-Length | awk '{print $2}')
+echo "Size of the response body: $response bytes"
+
